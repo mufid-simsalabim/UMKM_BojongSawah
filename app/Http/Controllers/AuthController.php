c484015bd0dfe3b99b9238c6802e1d559a2d69b5
irsanwoy <irsanwoy54@gmail.com>
2026-08-26 17:26:07 +0700

    Fix missing Category model import in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\UmkmProfile;
+use App\Models\Category;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
