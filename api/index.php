99ec1404f0087c0fea39217bc714300a824f5cd9
irsanwoy <irsanwoy54@gmail.com>
2026-08-26 16:22:39 +0700

    Fix Laravel storage path for Vercel serverless environment

--- api/index.php
+++ api/index.php
@@ -1,7 +1,9 @@
 <?php
 
-// Prepare storage subdirectiories in /tmp for Vercel serverless environment
+// Prepare storage subdirectories in /tmp for Vercel serverless environment
 $storageDir = '/tmp/storage';
+$cacheDir = $storageDir . '/bootstrap/cache';
+
 $dirs = [
     $storageDir . '/app/public',
     $storageDir . '/framework/cache/data',
@@ -9,6 +11,7 @@ $dirs = [
     $storageDir . '/framework/testing',
     $storageDir . '/framework/views',
     $storageDir . '/logs',
+    $cacheDir,
 ];
 
 foreach ($dirs as $dir) {
@@ -21,6 +24,17 @@ putenv("APP_STORAGE={$storageDir}");
 $_ENV['APP_STORAGE'] = $storageDir;
 $_SERVER['APP_STORAGE'] = $storageDir;
 
+putenv("APP_SERVICES_CACHE={$cacheDir}/services.php");
+putenv("APP_PACKAGES_CACHE={$cacheDir}/packages.php");
+putenv("APP_CONFIG_CACHE={$cacheDir}/config.php");
+putenv("APP_ROUTES_CACHE={$cacheDir}/routes.php");
+putenv("APP_EVENTS_CACHE={$cacheDir}/events.php");
+
+$_ENV['APP_SERVICES_CACHE'] = "{$cacheDir}/services.php";
+$_ENV['APP_PACKAGES_CACHE'] = "{$cacheDir}/packages.php";
+$_ENV['APP_CONFIG_CACHE'] = "{$cacheDir}/config.php";
+$_ENV['APP_ROUTES_CACHE'] = "{$cacheDir}/routes.php";
+$_ENV['APP_EVENTS_CACHE'] = "{$cacheDir}/events.php";
+
 // Forward all Vercel requests to Laravel entry point
 require __DIR__ . '/../public/index.php';
-
