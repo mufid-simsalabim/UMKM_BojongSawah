110a9ecac468b19d5274a864d55281fe2df6655a
mufid-simsalabim <raden.hadi06@gmail.com>
2026-08-26 22:27:22 +0700

    Fix password double-hashing bug on user/UMKM registration

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,7 +33,6 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
         ];
     }
 
